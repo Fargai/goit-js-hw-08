97ff6c72ee2fa74bc7393e1973fa3d0bfcc98923
Natalka Pashchuk <fargai@ukr.net>
2023-04-03 15:05:53 +0300

    Add video-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,43 @@
+var throttle = require('lodash.throttle');
+import Player from '@vimeo/player';
 
+const vimeoPlayer = document.querySelector('#vimeo-player');
+const player = new Player(vimeoPlayer);
+
+
+const onPlay = function (event) {
+localStorage.setItem('videoplayer-current-time', event.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const time = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(time);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+//     .then(function (seconds) {
+//     // seconds = the actual time that the player seeked to
+// }).catch(function(error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             // the time was less than 0 or greater than the video’s duration
+//             break;
+
+//         default:
+//             // some other error occurred
+//             break;
+//     }
+// });
