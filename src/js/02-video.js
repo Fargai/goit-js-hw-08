38b15a243e4caaab5f502fe95a90fc412b9df34a
Natalka Pashchuk <fargai@ukr.net>
2023-04-03 16:35:40 +0300

    add update 02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,4 @@
-var throttle = require('lodash.throttle');
+import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 
 const vimeoPlayer = document.querySelector('#vimeo-player');
@@ -14,30 +14,3 @@ player.on('timeupdate', throttle(onPlay, 1000));
 const time = localStorage.getItem('videoplayer-current-time');
 
 player.setCurrentTime(time);
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-//     .then(function (seconds) {
-//     // seconds = the actual time that the player seeked to
-// }).catch(function(error) {
-//     switch (error.name) {
-//         case 'RangeError':
-//             // the time was less than 0 or greater than the video’s duration
-//             break;
-
-//         default:
-//             // some other error occurred
-//             break;
-//     }
-// });
