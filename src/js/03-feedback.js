6dd1eaafbce46d406f3b58fbf283b74887d3c44c
Natalka Pashchuk <fargai@ukr.net>
2023-04-04 20:12:22 +0300

    Add 03-feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const emailInput = formEl.elements['email'];
+const messageInput = formEl.elements['message'];
+
+const dataForm = JSON.parse(localStorage.getItem('feedback-form-state'));
+if (dataForm) {
+emailInput.value = dataForm.email;
+messageInput.value = dataForm.message;
+};
+
+let valueForm = {
+email: '',
+message: '',
+};
+
+emailInput.addEventListener('input', throttle(e => {
+valueForm.email = e.target.value;
+localStorage.setItem('feedback-form-state', JSON.stringify(valueForm));
+}, 500)),
+
+messageInput.addEventListener('input', throttle(e => {
+valueForm.message = e.target.value;
+localStorage.setItem('feedback-form-state', JSON.stringify(valueForm));
+}, 500)),
+
+formEl.addEventListener('submit', e => {
+e.preventDefault();
+console.log(valueForm);
+localStorage.removeItem('feedback-form-state');
+emailInput.value = '';
+messageInput.value = '';
+});
